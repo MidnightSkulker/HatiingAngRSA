72da903b57bc0a733562fead454d560bcbe3738c
Peter White <desourdesourde@gmail.com>
2019-02-27 15:53:22 -0800

    First experiement with RSA

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,36 @@
 module Main where
 
+import Crypto.PubKey.RSA (PublicKey, PrivateKey, generate)
+import System.IO (readFile)
+import Data.Time (getCurrentTime)
+
+generateKey :: IO (PublicKey, PrivateKey)
+generateKey = do
+  (public, private) <- generate 64 3
+  return (public, private)
+
+printTime :: IO ()
+printTime = do
+  time <- getCurrentTime
+  putStrLn (show time)
+
+printConfig :: IO ()
+printConfig = do
+  contents <- readFile "stack.yaml"
+  putStrLn contents
+
+printNumbers :: IO ()
+printNumbers = do
+  putStrLn (show (3+4)) 
+
 main :: IO ()
 main = do
   putStrLn "hello world"
+  printNumbers
+  printConfig
+  printTime
+  (pu, pr) <- generateKey
+  putStrLn ("(Public, Private) = (" ++ show pu ++ ", " ++ show pr ++ ")\n")
+
+m :: IO ()
+m = main
